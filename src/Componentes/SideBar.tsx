d65d228b7edcc7d67297356427f52ad65c9d9f9c
Martin Alzueta <martin.alzueta@wheregroup.com>
2024-11-03 22:42:26 +0100

    menus como acordeones en la sideBar

--- src/Componentes/SideBar.tsx
+++ src/Componentes/SideBar.tsx
@@ -1,10 +1,50 @@
+import { useContext } from "react";
 
-import { AppBar} from "@mui/material";
-
-
+import {AppBar,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+    Typography,
+  } from "@mui/material";
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import "simplebar-react/dist/simplebar.min.css";
 
-export default function SideBar(props) {
+import ExportPDF from "./ExportPDF";
+import Tools from "./Tools";
+import DisplayInfos from "./DisplayInfo";
+import { AppContext } from "../contexto/AppContext";
+import { DataContext } from "../contexto/DataContext";
+
+
+export default function SideBar(props: any) {
+  const appContext = useContext(AppContext) as any;
+  const dataContext = useContext(DataContext) as any;
+
+  const menus = [{
+    title: "Filtros",
+    child: <Tools />,
+    icon: <FilterAltIcon />
+  }, {
+    title: "Exportar mapa",
+    child: <ExportPDF
+    titulo={
+      appContext.loteSeleccionado !== -1
+        ? dataContext.lotesList.filter(
+            (lote) => lote.id == appContext.loteSeleccionado
+          )[0].name
+        : dataContext.campo?.features[0].properties.name
+    }
+  />,
+  icon: <PictureAsPdfIcon />
+  }
+//   ,{
+//     title: "Información",
+//     child: <DisplayInfos />,
+//     icon: undefined 
+// } 
+];
+
   return (
     <>
       <AppBar
@@ -19,7 +59,18 @@ export default function SideBar(props) {
           width: props.open ? "256px" : "0px",
         }}
       >
-        {props.open && props.children}
+        {props.open && menus.map((item)=> <Accordion>
+              <AccordionSummary>
+                {item.icon}
+                <Typography variant="body2" sx={{paddingLeft: 1}}>{item.title}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+               {item.child}
+              </AccordionDetails>
+            </Accordion>)
+        
+       
+        }
       </AppBar>
     </>
   );
