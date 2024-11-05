f70cddaeada30b8fa9942f4d47b7cfcec6e0e0b1
Martin Alzueta <martin.alzueta@wheregroup.com>
2024-11-05 23:07:39 +0100

    filtro de semaforos con iconos

--- src/Componentes/Tools.tsx
+++ src/Componentes/Tools.tsx
@@ -1,73 +1,92 @@
 import { useContext, useEffect } from "react";
-import { InputLabel, Select, MenuItem, Switch} from "@mui/material";
+import {
+  InputLabel,
+  Grid,
+  IconButton,
+  MenuItem,
+  Select,
+  Switch,
+  Tooltip,
+} from "@mui/material";
 import { AppContext } from "../contexto/AppContext";
 import { DataContext } from "../contexto/DataContext";
 
+import SpaIcon from "@mui/icons-material/Spa";
+import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
+import AdbIcon from "@mui/icons-material/Adb";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import DoNotDisturbIcon from "@mui/icons-material/DoNotDisturb";
+
 const semaforos = [
-    { label: "ninguno", field: "ninguno" },
-    { label: "general", field: "general" },
-    { label: "enfermedades", field: "diseases" },
-    { label: "calidad de implantación", field: "implementation_quality" },
-    { label: "plagas", field: "pests" },
-    { label: "malezas", field: "weeds" },
-    { label: "adversidades", field: "adversities" },
-  ];
+  { label: "Ninguno", field: "ninguno", icon: <DoNotDisturbIcon /> },
+  { label: "General", field: "general", icon: <AssessmentIcon /> },
+  { label: "Enfermedades", field: "diseases", icon: <AdbIcon /> },
+  {
+    label: "Calidad de implantación",
+    field: "implementation_quality",
+    icon: <SpaIcon />,
+  },
+  { label: "Plagas", field: "pests", icon: <LocalHospitalIcon /> },
+  { label: "Malezas", field: "weeds", icon: <LocalFloristIcon /> },
+  { label: "Adversidades", field: "adversities", icon: <AcUnitIcon /> },
+];
 
+export default function Tools() {
+  const appContext = useContext(AppContext) as any;
+  const dataContext = useContext(DataContext) as any;
 
+  useEffect(() => {
+    appContext.setSemaforo(semaforos[0].field);
+    appContext.set;
+  }, []);
+  return (
+    <>
+      <InputLabel>Lote: </InputLabel>
+      <Select
+        id="plot_name"
+        label="lote"
+        value={appContext.loteSeleccionado}
+        onChange={(ev) =>
+          appContext.setLoteSeleccionado(ev.target.value as number)
+        }
+        fullWidth
+      >
+        {dataContext.campo &&
+          dataContext.lotesList.map((el) => (
+            <MenuItem key={el.id} value={el.id}>
+              {el.name}
+            </MenuItem>
+          ))}
+      </Select>
 
-export default function Tools(){
-
-const appContext = useContext(AppContext) as any;
-const dataContext = useContext(DataContext) as any;
-
-useEffect(()=>{
-    appContext.setSemaforo(semaforos[0].field)
-    appContext.set
-}, [])
-return (
-    
-    <> 
-
-        <InputLabel>Ver campo: </InputLabel>
-            <Switch
-              checked={appContext.showCampo}
-              onChange={() => appContext.setShowCampo(!appContext.showCampo)}
-            />
-            <InputLabel>Lote</InputLabel>
-            <Select
-              id="plot_name"
-              label="lote"
-              value={appContext.loteSeleccionado}
-              onChange={(ev) => appContext.setLoteSeleccionado(ev.target.value as number)}
-              fullWidth
+      <InputLabel>Semaforos: </InputLabel>
+      {semaforos.map((el) => {
+        return (
+          <Tooltip title={el.label}>
+            <IconButton
+              onClick={() => appContext.setSemaforo(el.field)}
+              color={appContext.semaforo === el.field ? "primary" : "secondary"}
+              size="large"
             >
-              {dataContext.campo &&              
-                dataContext.lotesList.map((el) => (
-                  <MenuItem key={el.id} value={el.id}>
-                    {el.name}
-                  </MenuItem>
-                ))}
-            </Select>
+              {el.icon}
+            </IconButton>
+          </Tooltip>
+        );
+      })}
 
-            <InputLabel>Semaforos</InputLabel>
-            <Select
-              id="semaforos"
-              label="semaforos"
-              defaultValue={appContext.semaforo}
-              onChange={(ev) => appContext.setSemaforo(ev.target.value as string)}
-              fullWidth
-            >
-              {semaforos.map((el) => {
-                return (
-                  <MenuItem key={el.label} value={el.field}>
-                    {el.label}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-            
-          </>   
-   
-)
-
+      <Grid container alignItems="center">
+        <Grid item xs={4}>
+          <InputLabel>Ver campo: </InputLabel>
+        </Grid>
+        <Grid item xs={8}>
+          <Switch
+            checked={appContext.showCampo}
+            onChange={() => appContext.setShowCampo(!appContext.showCampo)}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
